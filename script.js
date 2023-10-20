546913138c07b974868fd200f50ab0108c410cac
jonas.marschner@mailbox.org <jonas.marschner@mailbox.org>
2023-10-20 11:12:32 +0200

    Add UI to play the game

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+const container = document.querySelector(".container");
+
+let gameCounter = 0;
+let winCounter = 0;
+let lossCounter = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -6,13 +15,13 @@ function getComputerChoice() {
     let choice = "";
     switch (getRandomInt(3)) {
         case 0:
-            choice = "Rock";
+            choice = "rock";
             break;
         case 1:
-            choice = "Paper";
+            choice = "paper";
             break;
         case 2:
-            choice = "Scissors"
+            choice = "scissors"
             break;
         default:
             return choice;
@@ -21,36 +30,62 @@ function getComputerChoice() {
     return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
+function gameResult(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return "We have a tie!"
+        return "You both chose " + computerSelection + ". We have a tie!"
     } else if ((playerSelection == "rock" && computerSelection == "scissors") 
     || (playerSelection == "scissors" && computerSelection == "paper") 
     || (playerSelection == "paper" && computerSelection == "rock")) {
-        return "You won! " + playerSelection + " beats " + computerSelection;
+        return "You won, " + playerSelection + " beats " + computerSelection + "!";
     } else {
-        return "You lost! " + computerSelection + " beats " + playerSelection; 
+        return "You lost, " + computerSelection + " beats " + playerSelection + "!"; 
     }
 }
 
-function game() {
-    let wins = 0;
-    for(let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, paper or scissors?");
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result.includes("won")) {
-            wins++;
-        }
+function countingPoints(message) {
+
+    if (message.includes("won")) {
+        winCounter++;
     }
-    console.log("You have won " + wins + " time(s)")
-    
+
+    if (message.includes("lost")) {
+        lossCounter++;
+    }
+}
+
+function addingParagraph(text) {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    container.appendChild(paragraph);
+}
+
+function playRound(playerSelection) {
+    if(gameCounter == 0) {
+        container.replaceChildren();
+    }
+
+    gameCounter++
+
+    let computerSelection = getComputerChoice();
+
+    let message = gameResult(playerSelection, computerSelection);
+
+    countingPoints(message);
+
+    addingParagraph(message);
+
+    if(gameCounter == 5) {
+        let gameResultMessage = "You played 5 games. You " + ((winCounter > lossCounter) ? "won!" : "lost :(");
+        addingParagraph(gameResultMessage);
+        gameCounter = 0;
+    }
+}
+
+function main() {
+    rockButton.addEventListener("click", () => playRound("rock"));
+    paperButton.addEventListener("click", () => playRound("paper"));
+    scissorsButton.addEventListener("click", () => playRound("scissors"));
 
 }
 
-
-game();
+main();
